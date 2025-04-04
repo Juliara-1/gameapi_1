61aa5b452c775baaa6993e306734c3c70c76b2b8
Тимур Яйкаров <yaykarov@MacBook-Pro-Timur.local>
2025-04-04 16:36:22 +0300

    FS-002: fix settings

--- gameapi/settings.py
+++ gameapi/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'catalog',
-    'drf_yasg',
-    'rest_framework',    #добавила с GPT
+    'drf_yasg'
 ]
 
 REST_FRAMEWORK = {
